feaf222a943f40f42cf4d716263ac95677084c3d
ysh <livefor1212@gmail.com>
2021-04-25 23:29:27 +0900

    Initial commit

---
+++ src/makegrade/makeGrade.java
@@ -0,0 +1,15 @@
+package makegrade;
+
+import java.util.ArrayList;
+
+public class makeGrade {
+   public static void main(String[] args) {
+      String answer = "";
+      int scores[][] = {{20, 30, 22, 44, 90}, {40, 70}, {44, 44, 30, 22}, {44, 44, 20, 50, 50}};
+      ArrayList arrayList = new ArrayList();
+      // 0,1,2,3 학생 -> 자기가 자기점수매긴숫자가 최고나 최저이면 성적 평균에서 빼고 매긴다.
+      System.out.println("배열길이 : "+scores.length);
+
+      
+   }
+}
